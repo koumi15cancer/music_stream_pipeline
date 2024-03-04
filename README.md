6635c334ae1f60e648b869a7049cf58fe77f68b3
koumi15cancer <daiduong040700@gmail.com>
2024-03-05 03:38:57 +0800

    testing intergrate multipass repo support k8 setup

--- README.md
+++
@@ -1,2 +0,0 @@
-# music_stream_pipeline
-The project streams events generated from a fake music streaming service (Eventsim) and creates a data pipeline that consumes real-time data.
--- src/infrastructure/terraform/modules/infiscal/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "supbase_key" {
-  value = data.infisical_secrets.my-secrets.secrets[var.supbase_token].value
-}
-
-output "upstash_key" {
-  value = data.infisical_secrets.my-secrets.secrets[var.upstash_token].value
-}
